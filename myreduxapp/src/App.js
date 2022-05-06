44e3f959ec4f05df707171542f5d6ca08a0ccc1c
abhiyanrai <abhishek.rai8280@gmail.com>
2022-05-06 00:05:41 -0700

    Routing Navbar

---
+++ myreduxapp/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import NotFound from './components/NotFound';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/cart" exact element={<Cart/>}/>
+          <Route path="/not-found" element={<NotFound/>}/>
+          <Route path="/" exact element={<Home/>}/>
+          <Route path="*" element={<Navigate to="not-found"/>} />
+        </Routes>  
+        {/* <Navigate path ="/not-found" /> */}
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
